3747a51ea83f2be9054fd4fff6ff1ea193333d2f
lihonggui <15616643714@163.com>
2021-11-23 14:40:15 +0800

    create swift pro

---
+++ weibo/weibo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  weibo
+//
+//  Created by lihonggui on 2021/11/23.
+//  Copyright © 2021 lihonggui. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
